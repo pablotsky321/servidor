a76d45c7c0e1034eb4d0c3a6d3877c49a1a79ba0
pablotsky321 <jprestrepo346@gmail.com>
2023-10-12 18:25:10 -0500

    updated

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json())
 
-
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
@@ -26,8 +25,7 @@ db.connect((err) => {
 
   app.post('/api/productos', (req, res) => {
     const sql = 'SELECT * FROM producto'; 
-    const {ficticio}=req.body;
-    db.query(sql,[ficticio] ,(err, result) => {
+    db.query(sql,(err, result) => {
       if (err) {
         console.error('Error al obtener registros:', err);
         res.status(500).json({ error: 'Error al obtener registros' });
@@ -37,10 +35,10 @@ db.connect((err) => {
     });
   });
 
-  app.post('/api/detalle_producto', (req, res) => {
-    const {id_producto}=req.body
-    const sql=`select * from producto where id=?`;
-    db.query(sql,[id_producto] ,(err, result) => {
+  app.post('/api/productos/:id_producto', (req, res) => {
+    const id_producto=req.params.id_producto;
+    const sql=`select * from producto where id=${id_producto}`;
+    db.query(sql,(err, result) => {
       if (err) {
         console.error('Error al obtener registros:', err);
         res.status(500).json({ error: 'Error al obtener registros' });
